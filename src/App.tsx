66f3f98ef46d6cf0439b4a273172d385db2f6f51
Test-User-KL <lytkfc@outlook.com>
2025-11-20 13:02:31 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <footer>
+      <nav>
+        <button>Início</button>
+        <button>Disciplinas</button>
+      </nav>
+    </footer>
+  );
+}
