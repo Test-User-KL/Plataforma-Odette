410e27f62487f0eb8da418929be88645cdaced2f
Test-User-KL <leo.yukio.tanaka@gmail.com>
2025-11-27 09:42:14 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -373,49 +373,49 @@ export default function App() {
 			</main>
 
 			<nav className="main-nav">
-					<a
-						href="#home"
-						onClick={(e) => {
-							e.preventDefault();
-							changeTab("home");
-						}}
-					>
-						<i className="fa-solid fa-home"></i>
-						<span>Início</span>
-					</a>
+				<a
+					href="#home"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("home");
+					}}
+				>
+					<i className="fa-solid fa-home"></i>
+					<span>Início</span>
+				</a>
 
-					<a
-						href="#subjects"
-						onClick={(e) => {
-							e.preventDefault();
-							changeTab("subjects");
-						}}
-					>
-						<i className="fa-brands fa-microsoft"></i>
-						<span>Disciplinas</span>
-					</a>
+				<a
+					href="#subjects"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("subjects");
+					}}
+				>
+					<i className="fa-brands fa-microsoft"></i>
+					<span>Disciplinas</span>
+				</a>
 
-					<a
-						href="#settings"
-						onClick={(e) => {
-							e.preventDefault();
-							changeTab("settings");
-						}}
-					>
-						<i className="fa-solid fa-gear"></i>
-						<span>Configuração</span>
-					</a>
+				<a
+					href="#settings"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("settings");
+					}}
+				>
+					<i className="fa-solid fa-gear"></i>
+					<span>Configuração</span>
+				</a>
 
-					<a
-						href="#account"
-						onClick={(e) => {
-							e.preventDefault();
-							changeTab("account");
-						}}
-					>
-						<i className="fa-solid fa-user"></i>
-						<span>Conta</span>
-					</a>
+				<a
+					href="#account"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("account");
+					}}
+				>
+					<i className="fa-solid fa-user"></i>
+					<span>Conta</span>
+				</a>
 			</nav>
 		</div>
 	);
