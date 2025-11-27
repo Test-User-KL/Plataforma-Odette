392abd88998b92695fd1559530249500c0a63a58
Test-User-KL <leo.yukio.tanaka@gmail.com>
2025-11-27 10:47:13 -0300

    .

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,4 +1,5 @@
 import type { TabView } from "../types/tabs";
+import { LiquidGlass } from "./LiquidGlass.tsx";
 
 interface MainNavProps {
 	changeTab: (targetView: TabView) => void;
@@ -6,51 +7,53 @@ interface MainNavProps {
 
 export default function MainNav({ changeTab }: MainNavProps) {
 	return (
-		<nav className="main-nav">
-			<a
-				href="#home"
-				onClick={(e) => {
-					e.preventDefault();
-					changeTab("home");
-				}}
-				className="active"
-			>
-				<i className="fa-solid fa-home"></i>
-				<span>Início</span>
-			</a>
+		<LiquidGlass>
+			<nav className="main-nav">
+				<a
+					href="#home"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("home");
+					}}
+					className="active"
+				>
+					<i className="fa-solid fa-home"></i>
+					<span>Início</span>
+				</a>
 
-			<a
-				href="#subjects"
-				onClick={(e) => {
-					e.preventDefault();
-					changeTab("subjects");
-				}}
-			>
-				<i className="fa-brands fa-microsoft"></i>
-				<span>Disciplinas</span>
-			</a>
+				<a
+					href="#subjects"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("subjects");
+					}}
+				>
+					<i className="fa-brands fa-microsoft"></i>
+					<span>Disciplinas</span>
+				</a>
 
-			<a
-				href="#settings"
-				onClick={(e) => {
-					e.preventDefault();
-					changeTab("settings");
-				}}
-			>
-				<i className="fa-solid fa-gear"></i>
-				<span>Configuração</span>
-			</a>
+				<a
+					href="#settings"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("settings");
+					}}
+				>
+					<i className="fa-solid fa-gear"></i>
+					<span>Configuração</span>
+				</a>
 
-			<a
-				href="#account"
-				onClick={(e) => {
-					e.preventDefault();
-					changeTab("account");
-				}}
-			>
-				<i className="fa-solid fa-user"></i>
-				<span>Conta</span>
-			</a>
-		</nav>
+				<a
+					href="#account"
+					onClick={(e) => {
+						e.preventDefault();
+						changeTab("account");
+					}}
+				>
+					<i className="fa-solid fa-user"></i>
+					<span>Conta</span>
+				</a>
+			</nav>
+		</LiquidGlass>
 	);
 }
