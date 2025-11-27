55f076f6cc151c4568bb879d59b0e786a4d983f4
Test-User-KL <leo.yukio.tanaka@gmail.com>
2025-11-27 11:10:15 -0300

    .

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,5 +1,4 @@
 import type { TabView } from "../types/tabs";
-import { LiquidGlass } from "./LiquidGlass.tsx";
 
 interface MainNavProps {
 	changeTab: (targetView: TabView) => void;
@@ -7,53 +6,51 @@ interface MainNavProps {
 
 export default function MainNav({ changeTab }: MainNavProps) {
         return (
-                <LiquidGlass className="main-nav">
-                        <nav className="main-nav__content">
-                                <a
-                                        href="#home"
-                                        onClick={(e) => {
-                                                e.preventDefault();
-                                                changeTab("home");
-                                        }}
-                                        className="active"
-                                >
-                                        <i className="fa-solid fa-home"></i>
-                                        <span>Início</span>
-                                </a>
+			<nav className="main-nav">
+					<a
+							href="#home"
+							onClick={(e) => {
+									e.preventDefault();
+									changeTab("home");
+							}}
+							className="active"
+					>
+							<i className="fa-solid fa-home"></i>
+							<span>Início</span>
+					</a>
 
-                                <a
-                                        href="#subjects"
-                                        onClick={(e) => {
-                                                e.preventDefault();
-                                                changeTab("subjects");
-                                        }}
-                                >
-                                        <i className="fa-brands fa-microsoft"></i>
-                                        <span>Disciplinas</span>
-                                </a>
+					<a
+							href="#subjects"
+							onClick={(e) => {
+									e.preventDefault();
+									changeTab("subjects");
+							}}
+					>
+							<i className="fa-brands fa-microsoft"></i>
+							<span>Disciplinas</span>
+					</a>
 
-                                <a
-                                        href="#settings"
-                                        onClick={(e) => {
-                                                e.preventDefault();
-                                                changeTab("settings");
-                                        }}
-                                >
-                                        <i className="fa-solid fa-gear"></i>
-                                        <span>Configuração</span>
-                                </a>
+					<a
+							href="#settings"
+							onClick={(e) => {
+									e.preventDefault();
+									changeTab("settings");
+							}}
+					>
+							<i className="fa-solid fa-gear"></i>
+							<span>Configuração</span>
+					</a>
 
-                                <a
-                                        href="#account"
-                                        onClick={(e) => {
-                                                e.preventDefault();
-                                                changeTab("account");
-                                        }}
-                                >
-                                        <i className="fa-solid fa-user"></i>
-                                        <span>Conta</span>
-                                </a>
-                        </nav>
-                </LiquidGlass>
+					<a
+							href="#account"
+							onClick={(e) => {
+									e.preventDefault();
+									changeTab("account");
+							}}
+					>
+							<i className="fa-solid fa-user"></i>
+							<span>Conta</span>
+					</a>
+			</nav>
         );
 }
